0fd5e4313d6835322807636b76d39a54c2c07324
Matt Chaffe <matt@mattchaffe.uk>
2018-12-07 00:24:41 +0000

    Removal of initial code which is not used.

--- src/channels.js
+++ src/channels.js
@@ -37,16 +37,4 @@ module.exports = function (app) {
     // e.g. to publish all service events to all authenticated users use
     return app.channel('authenticated')
   })
-
-  // Here you can also add service specific event publishers
-  // e.g. the publish the `users` service `created` event to the `admins` channel
-  // app.service('users').publish('created', () => app.channel('admins'));
-
-  // With the userid and email organization from above you can easily select involved users
-  // app.service('messages').publish(() => {
-  //   return [
-  //     app.channel(`userIds/${data.createdBy}`),
-  //     app.channel(`emails/${data.recipientEmail}`)
-  //   ];
-  // });
 }
